aeebbe32c9a3d019ebeeaca3788050101f5ec279
turkeyzhu <turkeyzhu@gmail.com>
2022-10-19 15:03:40 +0800

    Add files via upload

--- model/VideoPlayer/src/VideoPlayer/Audio/PcmVolumeControl.h
+++ model/VideoPlayer/src/VideoPlayer/Audio/PcmVolumeControl.h
@@ -1,1 +1,22 @@
+﻿/**
+ * turkeyzhu
+ * http://blog.yundiantech.com/
+ */
 
+#ifndef PCMVOLUMECONTROL_H
+#define PCMVOLUMECONTROL_H
+
+class PcmVolumeControl
+{
+public:
+    PcmVolumeControl();
+
+    ///buf为需要调节音量的音频数据块首地址指针，size为长度，uRepeat为重复次数，通常设为1，vol为增益倍数,可以小于1
+    static void RaiseVolume(char* buf, int size, int uRepeat, double vol);
+
+};
+
+
+
+
+#endif // PCMVOLUMECONTROL_H
